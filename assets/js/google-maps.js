a7c43d2036877a49369097626ef4a8c34e1633f6
Emisael Kisler <ekisler@gmail.com>
2022-06-17 06:14:49 -0400

    goole maps

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
-/*function initMap() {
+function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 10.515139, lng: -66.952269};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,7 +10,6 @@
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Emisael Kisler, WD' // Title Location
     });
 }
-*/
